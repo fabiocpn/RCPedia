a36716a0f0ea67c56f77226921823c4915a9c00a
Fabio C. P. Navarro <fnavarro@webserver.compbio.ludwig.org.br>
2010-12-01 15:40:30 -0200

    New view/action for searching Retrogenes by coordinates

--- app/controllers/retrogenes_controller.php
+++ app/controllers/retrogenes_controller.php
@@ -13,6 +13,51 @@ class RetrogenesController extends AppController {
 		$this->set('retrogenes', $this->paginate('Retrogene', Array('Refseq.n_exons >' => 1,'Specie.name like' => "%$specie%")));
 	}
 
+	function searchbycoord() {
+		if(!isset($this->data) && $this->params['url']['url'] == "retrogenes/searchbycoord" ){
+			$this->Session->delete('coord');
+		} 
+		if(isset($this->data['Retrogenes']['coord_q'])) {
+			$array = preg_split("/:|-/", $this->data['Retrogenes']['coord_q']);
+
+			if ( !preg_match("/^chr/",$array[0]) ) {
+				#$this->Session->setFlash(sprintf(__('Invalid Seach', true), 'retrogene'));
+				$this->Session->setFlash("Invalid Search");
+				$this->redirect(array('action' => 'searchbycoord'));
+			}
+
+			if ( isset($array[0]) ) { 
+				$t_coord['chr']   = $array[0]; 
+			}
+			if ( isset($array[1]) ) { 
+				$t_coord['start'] = $array[1]; 
+			}
+			if ( isset($array[2]) ) { 
+				$t_coord['end']  = $array[2]; 
+			}
+
+			$this->Session->write('coord',$t_coord);
+	
+		}
+		$t_coord = $this->Session->read('coord');
+			if ( isset($t_coord['start']) && isset($t_coord['end']) ) { 
+					$this->paginate = array ( 'order' => 'Retrogene.g_start' );
+					$this->set('retrogenes', $this->paginate('Retrogene', Array('Refseq.n_exons >' => 1, 'Retrogene.chr' => $t_coord['chr'], 'Retrogene.g_start >=' => $t_coord['start'], 'Retrogene.g_end <=' => $t_coord['end'])));
+			}
+			else {
+				if ( !isset($t_coord['start']) && !isset($t_coord['end']) ) {
+					$this->paginate = array ( 'order' => 'Retrogene.g_start' );
+					$this->set('retrogenes', $this->paginate('Retrogene', Array('Refseq.n_exons >' => 1, 'Retrogene.chr' => $t_coord['chr'])));
+				
+				}	
+				else {
+					$this->Session->setFlash("Invalid Search");
+					$this->redirect(array('action' => 'searchbycoord'));
+				}
+			}
+		#$this->set('retrogenes', $this->paginate('Retrogene', Array('Refseq.n_exons >' => 1, 'Retrogene.chr' => $this->Session->read('coord')['chr'] )));
+	}
+
 	function index_debug() {
 		$this->Retrogene->recursive = 0;
 		$this->set('retrogenes', $this->paginate());
@@ -26,47 +71,5 @@ class RetrogenesController extends AppController {
 		$this->set('retrogene', $this->Retrogene->read(null, $id));
 	}
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->Retrogene->create();
-			if ($this->Retrogene->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'retrogene'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'retrogene'));
-			}
-		}
-	}
-
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'retrogene'));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Retrogene->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'retrogene'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'retrogene'));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Retrogene->read(null, $id);
-		}
-	}
-
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'retrogene'));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Retrogene->delete($id)) {
-			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Retrogene'));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Retrogene'));
-		$this->redirect(array('action' => 'index'));
-	}
 }
 ?>
