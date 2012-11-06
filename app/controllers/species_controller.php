87d6cd39065d891a9240aa6d79142cbd96fdea68
Fabio Navarro <fabiocpn@gmail.com>
2012-11-06 10:53:05 -0200

    Updated google chart API on species pages. Enhance select speed for the species data.

--- app/controllers/species_controller.php
+++ app/controllers/species_controller.php
@@ -15,32 +15,25 @@ class SpeciesController extends AppController {
 			$this->redirect(array('action' => 'index'));
 		}
 		$this->set('species', $this->Species->read(null, $id));
+
 		$this->set('genes',$this->Genes->find('count',array ( 'fields' => 'distinct(gene_name)','conditions' => array ( 'Genes.specie_id' => $id ), 'joins' => array ( array ( 'table' => 'refseqs', 'alias' => 'Refseq', 'type' => 'left', 'conditions' => array ( 'Refseq.gene_id = Genes.id') ) ) ) ) );
 
 		#Load retro count 
 		$this->set('retro',$this->Retrogenes->find('count',array ('conditions' => array ( 'Retrogenes.specie_id' => $id, 'Refseq.n_exons >' => 1 ), 'joins' => array ( array ( 'table' => 'refseqs', 'alias' => 'Refseq', 'type' => 'inner', 'conditions' => array ('Refseq.id = Retrogenes.refseq_id') ) ) ) ) );
 
-	#"http://chart.apis.google.com/chart?chs=350x225&cht=pc&chd=t:380,2311,987&chl=Exonic|Intronic|Intergenic&chtt=Retrogene+Genomic+Regions&chts=676767,12.5&chds=0,2311"
-	
 	#Load retro_region with the count of retrogene genomic regions
-		$this->set('retro_region',$this->Retrogenes->find('all',array ('conditions' => array ( 'Retrogenes.specie_id' => $id, 'Refseq.n_exons >' => 1 ), 'joins' => array ( array ( 'table' => 'refseqs', 'alias' => 'Refseq', 'type' => 'inner', 'conditions' => array ('Refseq.id = Retrogenes.refseq_id') ) ), 'group' => 'Retrogenes.g_region', 'fields' => array ('count(*) as count','Retrogenes.g_region') ) ) );
-
-		#pr($this->viewVars['retro_region']);
-		$chd="t:";
-		$chl="";
-		$max=0;
-		foreach ($this->viewVars['retro_region'] as $type):
-			if ( $type[0]['count'] > $max ) {
-				$max=$type[0]['count'];
-			}
-			$chd .= $type[0]['count'].",";
-			$chl .= $type['Retrogenes']['g_region']."|";
-		endforeach;
-		$chd=chop($chd,",");
-		$chtt="Retrogene+Genomic+Regions";
-		$chl=chop($chl,"|");
-		$chart = "http://chart.apis.google.com/chart?chs=350x225&cht=pc&chd=".$chd."&chl=".$chl."&chtt=".$chtt."&chts=676767,12.5&chds=0,".$max;
-		$this->set('char_gregion',$chart);
+		$this->Retrogenes->unbindModel(array('hasMany' => array('Expression')));
+		$this->Retrogenes->unbindModel(array('hasMany' => array('Conservation')));
+		$this->Retrogenes->unbindModel(array('belongsTo' => array('TGene')));
+		$this->Retrogenes->unbindModel(array('belongsTo' => array('Chr_accession')));
+		$this->Retrogenes->unbindModel(array('belongsTo' => array('Method')));
+		$this->Retrogenes->unbindModel(array('belongsTo' => array('Specie')));
+		$inter = $this->Retrogenes->find('count', Array('conditions' => Array( 'specie_id' => $id,'g_region' => 'Intergenic' )));
+		$intrasame = $this->Retrogenes->find('count', Array('conditions' => Array( 'specie_id' => $id,'g_region' => 'Intragenic (same strand)' )));
+		$intraoppo = $this->Retrogenes->find('count', Array('conditions' => Array( 'specie_id' => $id,'g_region' => 'Intragenic (opposite strand)' )));
+		$this->set('gregion_inter',$inter);
+		$this->set('gregion_intrasame',$intrasame);
+		$this->set('gregion_intraoppo',$intraoppo);
 
 	}
 }
--- app/views/species/view.ctp
+++ app/views/species/view.ctp
@@ -1,3 +1,50 @@
+
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load('visualization', '1', {packages: ['corechart']});
+    </script>
+    <script type="text/javascript">
+
+	  function init() {
+			drawVisualization_gregion();
+		    drawVisualization_count();
+	  }
+
+      function drawVisualization_gregion() {
+        // Create and populate the data table.
+        var data = google.visualization.arrayToDataTable([
+          ['Region', ''],
+          ['Intergenic', <?php echo $gregion_inter ?>],
+          ['Intragenic (opposite strand)', <?php echo $gregion_intraoppo?>],
+          ['Intragenic (same strand)', <?php echo $gregion_intrasame ?>]
+        ]);
+      
+        var options = {
+          'chartArea':{width:"100%"},
+          'legend':{'position': 'right', 'alignment':'center' },
+        };
+        // Create and draw the visualization.
+        new google.visualization.PieChart(document.getElementById('visualization')).
+            draw(data, options);
+      }
+      
+
+      function drawVisualization_count() {
+        // Create and populate the data table.
+        var data = google.visualization.arrayToDataTable([
+          ['', 'Genes', 'Retrocopies'],
+          ['',  <?php echo $genes ?>, <?php echo $retro ?>]
+        ]);
+      
+        // Create and draw the visualization.
+		        new google.visualization.ColumnChart(document.getElementById('visualization_genes')).
+            draw(data, { width:600, height:400, 'legend':{'position': 'bottom'} } );
+      }
+      
+
+      google.setOnLoadCallback(init);
+    </script>
+
 <div class="species view">
 <h2><?php  __('Species');?></h2>
 	<dl><?php $i = 0; $class = ' class="altrow"';?>
@@ -22,10 +69,22 @@
 			&nbsp;
 		</dd>
 	<br><br><br>
-	<?php echo $this->Html->image("http://chart.apis.google.com/chart?chxr=0,0,22500&chxt=y&chbh=a&chs=300x225&cht=bvg&chco=A2C180,3D7930&chds=0,22500,0,22500&chd=t:".$genes."|".$retro."&chdl=Gene|Retrogene&chma=|0,5&chtt=Retrogene+Proportion"); ?>&nbsp;
-	<?php echo $this->Html->image($char_gregion); ?>&nbsp;
-	<br><br><br>
+	<table><tr><td>
 	<center>
+	<font size=-2><b>
+	Genome summary</b></font></center><br>
+	<div id="visualization_genes" style="width: 500px; height: 400px;"></div>
+	</td><td>
+	<center>
+	<font size=-2><b>
+	Retrocopies genomic context</b></font></center><br>
+	<div id="visualization" style="width: 500px; height: 400px;"></div>
+	</td></tr>
+	</table>
+
+	<center>
+	<font size=-2><b>
+	Graphical representation of organism retrocopies</b></font><br>
 	<?php echo $this->Html->image("http://www.bioinfo.mochsl.org.br/~fnavarro/circos/circos.".strtolower($species['Species']['abreviation']).".png",Array("width" => 450,"height" => 450)); ?>
 	</center>
 	
